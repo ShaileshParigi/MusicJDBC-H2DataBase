08991ccf5eec843014911881cd526623a65f227e
ShaileshParigi <shaileshparigi13@gmail.com>
2023-03-04 17:18:32 +0000

    FinalSolution

--- src/main/java/com/example/song/model/Song.java
+++ src/main/java/com/example/song/model/Song.java
@@ -1,14 +1,14 @@
-// Write your code here
 package com.example.song.model;
 
-public class Song{
+public class Song {
+
     private int songId;
     private String songName;
     private String lyricist;
     private String singer;
     private String musicDirector;
 
-    public Song(int songId,String songName,String lyricist,String singer,String musicDirector){
+    public Song(int songId, String songName, String lyricist, String singer, String musicDirector) {
         this.songId = songId;
         this.songName = songName;
         this.lyricist = lyricist;
@@ -16,39 +16,43 @@ public class Song{
         this.musicDirector = musicDirector;
     }
 
-    public int getSongId(){
-        return this.songId;
+    public int getSongId() {
+        return songId;
     }
-    public void setSongId(int songId){
+
+    public void setSongId(int songId) {
         this.songId = songId;
     }
 
-    public String getSongName(){
-        return this.songName;
+    public String getSongName() {
+        return songName;
     }
-    public void setSongName(String songName){
+
+    public void setSongName(String songName) {
         this.songName = songName;
     }
 
-    public String getLyricist(){
-        return this.lyricist;
+    public String getLyricist() {
+        return lyricist;
     }
-    public void setLyricist(String lyricist){
+
+    public void setLyricist(String lyricist) {
         this.lyricist = lyricist;
     }
 
-    public String getSinger(){
-        return this.singer;
+    public String getSinger() {
+        return singer;
     }
-    public void setSinger(String singer){
+
+    public void setSinger(String singer) {
         this.singer = singer;
     }
 
-    public String getMusicDirector(){
-        return this.musicDirector;
-    }
-    public void setMusicDirector(String musicDirector){
-        this.musicDirector = musicDirector;
+    public String getMusicDirector() {
+        return musicDirector;
     }
 
+    public void setMusicDirector(String musicDirector) {
+        this.musicDirector = musicDirector;
+    }
 }
--- src/main/java/com/example/song/model/SongRowMapper.java
+++ src/main/java/com/example/song/model/SongRowMapper.java
@@ -1,29 +1,19 @@
-/*
- * 
- * You can use the following import statements
- * import org.springframework.jdbc.core.RowMapper;
- * import java.sql.ResultSet;
- * import java.sql.SQLException;
- * 
- */
-
-// Write your code here
 package com.example.song.model;
 
+import com.example.song.model.Song;
 import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.example.song.model.Song;
+public class SongRowMapper implements RowMapper<Song> {
 
-public class SongRowMapper implements RowMapper<Song>{
-    public Song mapRow(ResultSet rs,int rowNum) throws SQLException{
+    public Song mapRow(ResultSet rs, int rowNum) throws SQLException {
         return new Song(
-            rs.getInt("songId"),
-            rs.getString("songName"),
-            rs.getString("lyricist"),
-            rs.getString("singer"),
-            rs.getString("musicDirector")
-        );
+                rs.getInt("songId"),
+                rs.getString("songName"),
+                rs.getString("lyricist"),
+                rs.getString("singer"),
+                rs.getString("musicDirector"));
+
     }
 }
